7af33a98f11e4c69cc3c438a2969972bd6ad7d61
T.Serck <timserck@gmail.com>
2016-09-20 15:22:52 +0200

    'gallery_1'

--- dist/inc/gallery.inc.php
+++ dist/inc/gallery.inc.php
@@ -4,4 +4,10 @@
 		<label for="tableauTitre">type a title</label>
 		<input type="text" id='tableauTitre'>
 	</form>
+	<div class="gallery__content">
+		<figure>
+			<img src="" alt="">
+			<caption>titre description</caption>
+		</figure>
+	</div>
 </section>
