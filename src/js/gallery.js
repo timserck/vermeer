9d37c16f98199963d3bdf7a0909602c0cbe36f9f
T.Serck <timserck@gmail.com>
2016-09-21 11:08:37 +0200

    'filterWork'

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,25 +1,68 @@
-//galery trie 
+//gallery trie 
+////////////////////////////////
 
-//varrable
+//https://www.grafikart.fr/tutoriels/jquery/fuzzy-search-314
+
+//////////////////////////////////////
+//varriable
 var 
-tableauTitreVal ,
-tableauTitreInput = $( "#tableauTitre" );
+tableauTitreVal,//input value
+figcaption,//title of item
+regexp,// regExp
+result,// result of the match of the regex
+//string,// string match regex to add hilighted
+galleryContent = $( "#gallery__content" ),
+galleryContentItem = $( "#gallery__content li" ),
+galleryContentItemTitle = $( "#gallery__content figcaption" ),
+tableauFilter = $( "#tableauFilter" );
 
 
-var getTitreInput = function(){
-	tableauTitreInput
+	tableauFilter
   .keyup(function() {
     tableauTitreVal = $( this ).val();
-      console.log(tableauTitreVal);
-  });
+     if (tableauTitreVal == '') {
+     	galleryContentItem.show();
+     	//galleryContentItemTitle.removeClass('highlighted');
+     	return true;
 
-}
+     }
+       var regexp = '\\b(.*)';
+       for(var i in tableauTitreVal){
+       	regexp += '('+tableauTitreVal[i]+')(.*)'
+       }
+       regexp +='\\b';
+      galleryContentItem.show();
+     galleryContent.find('figure>figcaption').each(function() {
+     	var figcaption = $(this);
 
-var ShowImg = function(tableauTitreVal){
+     	//console.log(figcaption);
+     	 var result = figcaption.text().match(new RegExp(regexp,'i'));
+     	 //console.log(result);
+     	 if (result) {
+     	 // 	var string = '';
+     	 // 	for (var i in result) {
+     	 // 		if (i > 0) {
+     	 // 			if (i%2 == 0) {
+     	 // 				string += '<span class="highlighted">'+ result[i] + '</span>'
+     	 // 			}
+     	 // 			else{
+     	 // 				string += result[i];
+     	 // 			}
+     	 			
+     	 // 		}
+     	 // 	}//forend
+     	 // figcaption.empty().append(string);
+     	 }//if end
+     	 else{
+     	 	figcaption.parent().parent().hide();
+     	 }
 
-}
+     });//eachend
+
+  });//keyUpend
 
 
-getTitreInput();
+
+
 
 
